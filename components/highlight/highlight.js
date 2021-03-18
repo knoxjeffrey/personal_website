bc523cee24d1d1a1cb56323e0920c9f88c3dd91f
Jeff Knox <knoxjeffrey@outlook.com>
2021-03-18 00:31:51 +0000

    style changes

--- components/highlight/highlight.js
+++ components/highlight/highlight.js
@@ -1,7 +1,9 @@
 import hljs from "highlight.js/lib/core";
 import css from "highlight.js/lib/languages/css";
+import erb from "highlight.js/lib/languages/erb";
 
 hljs.registerLanguage("css", css);
+hljs.registerLanguage("erb", erb);
 
 document.addEventListener("turbo:load", function() {
   document.querySelectorAll("pre code").forEach(block => {
