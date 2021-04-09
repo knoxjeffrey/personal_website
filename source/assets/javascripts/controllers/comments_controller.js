859f1d980e08ae40cb2690859756e02fee6f42aa
Jeff Knox <knoxjeffrey@outlook.com>
2021-04-09 11:49:43 +0100

    fix comment spacing

--- source/assets/javascripts/controllers/comments_controller.js
+++ source/assets/javascripts/controllers/comments_controller.js
@@ -1,8 +1,11 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  static targets = [ "load", "commento" ]
+
   load() {
-    this.element.style.display = "none"
+    this.loadTarget.style.display = "none"
+    this.commentoTarget.classList.add("margin-top--xl")
     window.commento.main()
   }
 }
