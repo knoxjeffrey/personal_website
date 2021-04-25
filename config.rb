90484529c8448e74c671c5c252aa962eb34efab8
Jeff Knox <knoxjeffrey@outlook.com>
2021-04-25 18:51:16 +0100

    css compiled by webpack

--- config.rb
+++ config.rb
@@ -8,9 +8,9 @@ page "/*.xml", layout: false
 page "/*.json", layout: false
 page "/*.txt", layout: false
 
-config[:css_dir]      = "assets/stylesheets"
+config[:css_dir]      = "compiled-assets"
 config[:images_dir]   = "assets/images"
-config[:js_dir]       = "assets/javascripts"
+config[:js_dir]       = "compiled-assets"
 
 # activate all components
 Pathname.new("./components").children.each do |entry|
