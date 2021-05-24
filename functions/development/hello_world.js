0112c87ba7cd1233675ac778b3d78a8e7454d44d
Jeff Knox <knoxjeffrey@outlook.com>
2021-05-24 20:51:15 +0100

    expand demo function to use utils folder

--- functions/development/hello_world.js
+++ functions/development/hello_world.js
@@ -1,6 +1,8 @@
+import { randomNumberGenerator } from "/utils/random_number_generator"
+
 export async function handler(event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: `Hello world ${Math.floor(Math.random() * 10)}` })
+    body: JSON.stringify({ message: `Hello world ${randomNumberGenerator()}` })
   };
 }
