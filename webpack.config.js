414fb5c466f707db076752582a6e0bcb2f0219f7
Jeff Knox <knoxjeffrey@outlook.com>
2021-05-28 20:26:38 +0100

    remove the loader

--- webpack.config.js
+++ webpack.config.js
@@ -54,14 +54,6 @@ module.exports = function (env) {
     module: {
       rules: [
         {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: "esbuild-loader",
-          options: {
-            target: "es2015"
-          }
-        },
-        {
           test: /\.(sa|sc|c)ss$/,
           use: [
             MiniCssExtractPlugin.loader,
