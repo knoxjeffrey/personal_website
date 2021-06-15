4579d98af85c9d2496b1bc400ea8a90100e85586
Jeff Knox <knoxjeffrey@outlook.com>
2021-06-15 22:09:32 +0100

    extract more info in the function

--- functions/rum_logger-background.js
+++ functions/rum_logger-background.js
@@ -1,3 +1,16 @@
+import Bowser from "bowser"
+
 export async function handler(event, context) {
-  console.log(JSON.parse(event.body))
+  const rumMetrics = JSON.parse(event.body)
+  const browser = Bowser.parse(rumMetrics.userAgent);
+  console.log(event)
+  console.log(context)
+  const rumMetricsUpdated = { 
+    ...rumMetrics,
+    browserName: browser.browser.name,
+    osName: browser.os.name,
+    platformType: browser.platform.type,
+    sourceIp: event.requestContext.identity.sourceIp,
+  }
+  console.log(rumMetricsUpdated)
 }
