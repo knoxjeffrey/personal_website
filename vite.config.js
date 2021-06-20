d95741c7dc2c118570463840d03970cbf1413384
Jeff Knox <knoxjeffrey@outlook.com>
2021-06-21 00:59:14 +0100

    production and development settings

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,47 @@
 import esbuild from "rollup-plugin-esbuild"
 import globImport from "rollup-plugin-glob-import"
-import path from "path"
 
-export default {
-  build: {
-    brotliSize: false,
-    emptyOutDir: true,
-    minify: "esbuild",
-    outDir: ".tmp/dist/assets",
-    rollupOptions: {
-      input: {
-        "components": path.resolve(__dirname, "./source/assets/javascripts/components.js"),
-        "main": path.resolve(__dirname, "./source/assets/javascripts/main.js"),
-        "game": path.resolve(__dirname, "./source/assets/javascripts/game/game.js"),
-        "commento_css": path.resolve(__dirname, "./source/assets/stylesheets/commento.css"),
-        "components_css": path.resolve(__dirname, "./source/assets/stylesheets/components.css"),
-        "game_css": path.resolve(__dirname, "./source/assets/stylesheets/game.css"),
-        "main_css": path.resolve(__dirname, "./source/assets/stylesheets/main.css"),
-      },
-      output: {
-        assetFileNames: "[name].[ext]",
-        chunkFileNames: "[name].js",
-        entryFileNames: "[name].js",
-        format: "es"
-      },
-      plugins: [
-        esbuild({
-          target: "es2015"
-        }),
-        globImport()
-      ]
+export default ({ command, mode }) => {
+  let minifySetting
+  
+  if (mode === "development") {
+    minifySetting = false  
+  } else {
+    minifySetting = "esbuild" 
+  }
+
+  return {
+    build: {
+      brotliSize: false,
+      emptyOutDir: true,
+      minify: minifySetting,
+      outDir: ".tmp/dist/assets",
+      rollupOptions: {
+        input: {
+          "components": "./source/assets/javascripts/components.js",
+          "main": "./source/assets/javascripts/main.js",
+          "game": "./source/assets/javascripts/game/game.js",
+          "commento_css": "./source/assets/stylesheets/commento.css",
+          "components_css": "./source/assets/stylesheets/components.css",
+          "game_css": "./source/assets/stylesheets/game.css",
+          "main_css": "./source/assets/stylesheets/main.css",
+        },
+        output: {
+          assetFileNames: "[name].css",
+          chunkFileNames: "[name].js",
+          entryFileNames: "[name].js",
+          format: "es"
+        },
+        plugins: [
+          esbuild({
+            target: "es2015"
+          }),
+          globImport()
+        ],
+      }
+    },
+    server: {
+      port: "3333"
     }
-  },
-  server: {
-    port: "3333"
   }
 }
