6e4ad8bde8491c9d3c8705b6bc0b10bd37941386
Jeff Knox <knoxjeffrey@outlook.com>
2021-06-23 00:40:49 +0100

    fire prism on turbo load

--- components/highlight/highlight.js
+++ components/highlight/highlight.js
@@ -1,3 +1,7 @@
 import prism from "prismjs"
 import "prismjs/components/prism-json"
 import "prismjs/components/prism-ruby"
+
+document.addEventListener("turbo:load", function(event) {
+  prism.highlightAll()
+})
