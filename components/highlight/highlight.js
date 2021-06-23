afe4423a89f243a15da48ac6011652d98db65181
Jeff Knox <knoxjeffrey@outlook.com>
2021-06-23 21:04:29 +0100

    update prismjs styling

--- components/highlight/highlight.js
+++ components/highlight/highlight.js
@@ -1,6 +1,8 @@
 import prism from "prismjs"
+import "prismjs/components/prism-bash"
 import "prismjs/components/prism-json"
 import "prismjs/components/prism-ruby"
+import "prismjs/components/prism-yaml"
 
 document.addEventListener("turbo:load", event => {
   prism.highlightAll()
