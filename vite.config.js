2ea82c4ba669c40569d02c355a5cfaeeb62c2d64
Jeff Knox <knoxjeffrey@outlook.com>
2021-06-27 22:28:40 +0100

    esbuild in production

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
+import esbuild from "rollup-plugin-esbuild"
 import { defineConfig } from "vite"
 import RubyPlugin from "vite-plugin-ruby"
 
 export default defineConfig({
+  build: {
+    brotliSize: false,
+    emptyOutDir: true,
+    minify: "esbuild"
+  },
   plugins: [
+    esbuild({
+      target: [
+        "chrome64",
+        "firefox62",
+        "safari11.1",
+        "edge79"
+      ]
+    }),
     RubyPlugin(),
   ],
 })
+
