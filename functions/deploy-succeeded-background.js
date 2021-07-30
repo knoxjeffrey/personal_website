d6ed48d4c85ab2af14fc8a41cae7a18e2481d099
Jeff Knox <knoxjeffrey@outlook.com>
2021-07-30 21:59:33 +0100

    log the payload

--- functions/deploy-succeeded-background.js
+++ functions/deploy-succeeded-background.js
@@ -36,7 +36,7 @@ const sample = arr => arr[Math.floor(Math.random() * arr.length)]
 const randomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
 
 export async function handler(event, _context) {
-  console.log(event.body.payload)
+  console.log(JSON.parse(event.body))
 
 
   // let dataToInsert = {}
