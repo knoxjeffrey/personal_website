cda2980ead76a6c5ee2165a8c69d12e1d0050a73
Jeff Knox <knoxjeffrey@outlook.com>
2021-08-08 19:31:02 +0100

    successful builds updated

--- frontend/controllers/dashboard/successful_builds_controller.js
+++ frontend/controllers/dashboard/successful_builds_controller.js
@@ -2,12 +2,37 @@ import { Controller } from "stimulus"
 import { subscription } from "~/javascripts/store/mixins/subscription"
 
 export default class extends Controller {
+  static targets = [ "production", "deployPreview", "cms" ]
+
   connect() {
     subscription(this)
     this.subscribe()
   }
 
+  updateProductionValue() {
+    this.productionTarget.innerHTML = `Production ... ${this.calculateNumberOfBuilds("production")}`
+  }
+
+  updateDeployPreviewValue() {
+    this.deployPreviewTarget.innerHTML = `Depoly preview ... ${this.calculateNumberOfBuilds("deploy-preview")}`
+  }
+
+  updateCmsValue() {
+    this.cmsTarget.innerHTML = `CMS ... ${this.calculateNumberOfBuilds("cms")}`
+  }
+
+  calculateNumberOfBuilds(context) {
+    return this.store().selectedNetlifyBuildData.reduce((acc , data) => {
+      return data.context === context ? acc + 1 : acc
+    }, 0)
+  }
+
   storeUpdated(store, prop) {
+    if (prop === "selectedNetlifyBuildData") {
+      this.updateProductionValue()
+      this.updateDeployPreviewValue()
+      this.updateCmsValue()
+    }
   }
 
   disconnect() {
