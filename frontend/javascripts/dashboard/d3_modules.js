46ad3f140343d70b3ea142872efb209d603de392
Jeff Knox <knoxjeffrey@outlook.com>
2021-08-11 00:03:26 +0100

    different way to import d3 modules

--- frontend/javascripts/dashboard/d3_modules.js
+++ frontend/javascripts/dashboard/d3_modules.js
@@ -1,19 +1,19 @@
-import { 
-  select, scaleLinear, max, extent, axisLeft, axisBottom, line, curveMonotoneX, pointer,
-  interpolateString, leastIndex, timeFormat
-} from "d3"
+import { extent, leastIndex, max } from "d3-array"
+import { axisBottom, axisLeft } from "d3-axis"
+import { interpolateString } from "d3-interpolate"
+import { scaleLinear } from "d3-scale"
+import { pointer, select } from "d3-selection"
+import { curveMonotoneX, line } from "d3-shape"
+import { timeFormat } from "d3-time-format"
+import { transition } from "d3-transition"
 
 export default {
-  select,
-  scaleLinear,
-  max,
-  extent,
-  axisLeft,
-  axisBottom,
-  line,
-  curveMonotoneX,
-  pointer,
+  extent, leastIndex, max,
+  axisBottom, axisLeft,
   interpolateString,
-  leastIndex,
-  timeFormat
+  scaleLinear,
+  pointer, select,
+  curveMonotoneX, line,
+  timeFormat,
+  transition
 }
