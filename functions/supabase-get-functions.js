e777b63e7b40048705d659ac84953f2ce04349c0
Jeff Knox <knoxjeffrey@outlook.com>
2021-08-17 21:09:09 +0100

    dev now matching production

--- functions/supabase-get-functions.js
+++ functions/supabase-get-functions.js
@@ -17,10 +17,8 @@ export async function handler(event, _context) {
   if (functionRequested == "netlify_deploy_data_years_and_months") {
     const { data, error } = await supabase
       .rpc("netlify_deploy_data_years_and_months")
-    console.log(data)
     if (error) return { statusCode: 500, body: `An error occurred: ${JSON.stringify(error)}` }
-    
-    const payload = CONTEXT === "dev" ? data : data[0].netlify_deploy_data_years_and_months
-    return { statusCode: 200, body: JSON.stringify(payload) }
+
+    return { statusCode: 200, body: JSON.stringify(data) }
   }
 }
