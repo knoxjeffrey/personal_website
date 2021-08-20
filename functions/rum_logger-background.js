8c89f48b202dcad540bc81f38a254eecc08ecf85
Jeff Knox <knoxjeffrey@outlook.com>
2021-08-20 15:50:22 +0100

    only allow storing rum data in netlify dev or on production

--- functions/rum_logger-background.js
+++ functions/rum_logger-background.js
@@ -6,26 +6,27 @@ const {
   CONTEXT
 } = process.env
 
-// const prodHost = "www.jeffreyknox.dev"
+const prodHost = "www.jeffreyknox.dev"
 
 export async function handler(event, context) {
-  const rumEventLogs = JSON.parse(event.body)
+  if (CONTEXT === "dev" || event.headers.host === prodHost) {
+    const rumEventLogs = JSON.parse(event.body)
 
-  const updatedRumEventLogs = rumEventLogs.map(log => {
-    const { identifier, timeZone, ...strippedRumMetrics } = log;
-    const browser = Bowser.parse(log.userAgent);
+    const updatedRumEventLogs = rumEventLogs.map(log => {
+      const { identifier, timeZone, ...strippedRumMetrics } = log;
+      const browser = Bowser.parse(log.userAgent);
 
-    return { 
-      ...strippedRumMetrics,
-      identifier: shaJS("sha256").update(`${log.identifier}${log.userAgent}`).digest("hex"),
-      country: countryFromTimeZone(log.timeZone),
-      browserName: browser.browser.name,
-      osName: browser.os.name,
-      platformType: browser.platform.type
-    }
-  })
-  
-  console.log(updatedRumEventLogs)
-  console.log(CONTEXT)
-  return { statusCode: 200 }
+      return { 
+        ...strippedRumMetrics,
+        identifier: shaJS("sha256").update(`${log.identifier}${log.userAgent}`).digest("hex"),
+        country: countryFromTimeZone(log.timeZone),
+        browserName: browser.browser.name,
+        osName: browser.os.name,
+        platformType: browser.platform.type
+      }
+    })
+    
+    console.log(updatedRumEventLogs)
+    return { statusCode: 200 }
+  }
 }
