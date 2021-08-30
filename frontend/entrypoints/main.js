8fb23b68d53e9b37e39a625bd4fca322890b05ec
Jeff Knox <knoxjeffrey@outlook.com>
2021-08-30 17:44:49 +0100

    simplyfy the code and make use of sendBeacon

--- frontend/entrypoints/main.js
+++ frontend/entrypoints/main.js
@@ -1,11 +1,11 @@
-import { pushr } from "~/javascripts/pushr"
+import { pushrInit } from "~/javascripts/pushr"
 import * as Turbo from "@hotwired/turbo"
 import { Application } from "stimulus"
 
 import CommentsController from "~/controllers/comments_controller"
 import RUMController from "~/controllers/rum_controller"
 
-pushr.init()
+pushrInit()
 const application = Application.start()
 application.register("comments", CommentsController)
 application.register("rum", RUMController)
--- frontend/javascripts/pushr/dispatchers/rum.js
+++ frontend/javascripts/pushr/dispatchers/rum.js
@@ -3,80 +3,67 @@
  * @description Handles the pushr events for Real User Metrics
  */
 
-export const rum = (() => {
-  let _isRumDispatching = false
-  let _rumDispatchTimeout = undefined
-  let _allowedRumMetrics = ["ttfb", "networkInfo", "fcp", "fid", "lcp", "cls"]
-  let _rumMetricsCollected = []
-  let _rumObjects = []
+let isRumDispatching = false
+let rumDispatchTimeout = undefined
+let allowedRumMetrics = ["ttfb", "networkInfo", "fcp", "fid", "lcp", "cls"]
+let rumMetricsCollected = []
+let rumObjects = []
 
-  /**
-   * If it is a rum event then push the rum data onto a new array for rum data. If the metric type isn't
-   * one that is allow then we exit. If we have already collected it then we exit. A timeout will
-   * be created if one is not already running. This timeout will allow for multiple logs that are fired
-   * quickly, to be batched up and dispatched in one request rather than firing multiple requests.
-   * 
-   * @function pushrDispatcher
-   * @memberof javascripts.pushr.dispatchers.rum
-   */
-  const pushrDispatcher = (pushrObject) => {
-    if (pushrObject.event !== "rum") return
-    if (!_allowedRumMetrics.some(metric => metric === pushrObject.data.metric)) return
-    if (_rumMetricsCollected.some(metric => metric === pushrObject.data.metric)) return
-    
-    _rumMetricsCollected.push(pushrObject.data.metric)
-    _rumObjects.push(pushrObject.data)
-    if (_isRumDispatching === false) {
-      _isRumDispatching = true
-      _rumDispatchTimeout = setTimeout(_batchPostRumData, 500)
-    }
+/**
+ * If it is a rum event then push the rum data onto a new array for rum data. If the metric type isn't
+ * one that is allow then we exit. If we have already collected it then we exit. A timeout will
+ * be created if one is not already running. This timeout will allow for multiple logs that are fired
+ * quickly, to be batched up and dispatched in one request rather than firing multiple requests.
+ * 
+ * @function pushrDispatcher
+ * @memberof javascripts.pushr.dispatchers.rum
+ */
+export const pushrDispatcher = (pushrObject) => {
+  if (pushrObject.event !== "rum") return
+  if (!allowedRumMetrics.some(metric => metric === pushrObject.data.metric)) return
+  if (rumMetricsCollected.some(metric => metric === pushrObject.data.metric)) return
+  
+  rumMetricsCollected.push(pushrObject.data.metric)
+  rumObjects.push(pushrObject.data)
+  if (isRumDispatching === false) {
+    isRumDispatching = true
+    rumDispatchTimeout = setTimeout(batchPostRumData, 500)
   }
+}
 
-  /**
-   * Dispatch any remaining logs in _rumObjects when leaving the page
-   * 
-   * @function turboDispatcher
-   * @memberof javascripts.pushr.dispatchers.rum
-   */
-  const turboDispatcher = () => {
-    clearTimeout(_rumDispatchTimeout)
-    if (_rumObjects.length) _batchPostRumData()
-  }
+/**
+ * Dispatch any remaining logs in rumObjects when leaving the page
+ * 
+ * @function leavePageDispatcher
+ * @memberof javascripts.pushr.dispatchers.rum
+ */
+export const leavePageDispatcher = () => {
+  clearTimeout(rumDispatchTimeout)
+  if (rumObjects.length) batchPostRumData()
+}
 
-  /**
-   * When the rum data is sent, empty _rumObjects and change the _isRumDispatching to say the timeout
-   * is over
-   * 
-   * @function _batchPostRumData
-   * @memberof javascripts.pushr.dispatchers.rum
-   */
-  const _batchPostRumData = () => {
-    _postRumData(_rumObjects)
-    _rumObjects.length = 0
-    _isRumDispatching = false
-  }
+/**
+ * When the rum data is sent, empty rumObjects and change the isRumDispatching to say the timeout
+ * is over
+ * 
+ * @function batchPostRumData
+ * @memberof javascripts.pushr.dispatchers.rum
+ */
+const batchPostRumData = () => {
+  postRumData(rumObjects)
+  rumObjects.length = 0
+  isRumDispatching = false
+}
 
-  /**
-   * Post the rum data to a Netlify background function
-   * 
-   * @function _postRumData
-   * @memberof javascripts.pushr.dispatchers.rum
-   */
-  const _postRumData = rumData => {
-    fetch("/.netlify/functions/rum_logger-background", { 
-      method: "POST",
-      body: JSON.stringify(rumData)
-    })
-      .then(responseCheck => {
-        if (!responseCheck.ok) { throw Error(responseCheck.status); }
-      })
-      .catch(error => {
-        console.warn(error)
-      })
-  }
-
-  return {
-    pushrDispatcher,
-    turboDispatcher
-  }
-})()
+/**
+ * Post the rum data to a Netlify background function
+ * 
+ * @function postRumData
+ * @memberof javascripts.pushr.dispatchers.rum
+ */
+const postRumData = rumData => {
+  navigator.sendBeacon(
+    "/.netlify/functions/rum_logger-background",
+    JSON.stringify(rumData)
+  )
+}
