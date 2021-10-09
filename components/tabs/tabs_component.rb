1fc721ad3bd2f32d09a69881ed03dafc7d1fd321
Jeff Knox <knoxjeffrey@outlook.com>
2021-10-09 17:45:26 +0100

    tab setup before styling

---
+++ components/tabs/tabs_component.rb
@@ -0,0 +1,33 @@
+module Components
+  module Tabs
+    class TabsComponent < Middleman::Extension
+      helpers do
+        def tabs(&block)
+          concat(
+            content_tag(:div, class: "terminal-tabs") do 
+              content_tag(:ul) do
+                capture_html(&block)
+              end
+            end
+          )
+        end
+
+        def tab_list_item(opts)
+          content_tag(:li, opts[:text], build_list_html(opts))
+        end
+
+        private
+
+        def build_list_html(opts)
+          active = opts[:active] == true ? " active" : ""
+          classes = opts.dig(:html, :class) ? "menu-item#{active} #{opts[:html][:class]}" : "menu-item#{active}"
+          opts[:html] ||= {}
+          opts[:html][:class] = classes
+          opts[:html]
+        end
+      end
+    end
+  end
+end
+
+::Middleman::Extensions.register(:tabs_component, Components::Tabs::TabsComponent)
